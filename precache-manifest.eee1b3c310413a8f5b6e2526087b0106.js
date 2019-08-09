4c1265d7a502fc4a444339b3558db5b1a2919878
keyberthRengel <keyrengel24@gmail.com>
2019-08-09 22:49:23 +0200

    primer commit

---
+++ precache-manifest.eee1b3c310413a8f5b6e2526087b0106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2dc4f089fc4c7213a16d58cc5a4b43",
+    "url": "/recetas/index.html"
+  },
+  {
+    "revision": "04dc8bb05ecfcd0bd079",
+    "url": "/recetas/static/css/main.3f50a0c4.chunk.css"
+  },
+  {
+    "revision": "14bb3f5f434e3d3acf38",
+    "url": "/recetas/static/js/2.53bdbe53.chunk.js"
+  },
+  {
+    "revision": "04dc8bb05ecfcd0bd079",
+    "url": "/recetas/static/js/main.273cae34.chunk.js"
+  },
+  {
+    "revision": "9d158fd2109dfbf0aa95",
+    "url": "/recetas/static/js/runtime~main.2e29b543.js"
+  }
+]);
